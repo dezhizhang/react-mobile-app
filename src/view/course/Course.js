2b9df6a61cdc43354d46f9e485f8d5eb0a981d29
dezhizhang <1541609448@qq,com>
2018-07-23 00:41:12 +0800

    登录3D功卡片

--- src/view/course/Course.js
+++ src/view/course/Course.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Header from '../../common/components/Header'
+import Footer from '../../common/components/Header'
 
 class Course extends React.Component{
     render(){
         return (<div>
-            我是课程页
+           <Header
+                login={true}
+                menu={true}
+           />
+           <Footer/>
         </div>)
     }
 }
--- src/view/login/Login.js
+++ src/view/login/Login.js
@@ -1,8 +1,22 @@
-import React from 'react'
-
+import React from 'react';
+import Header from '../../common/components/Header';
+import './Login.css'
 class Login extends React.Component{
     render(){
-        return (<div>我是登录页</div>)
+        return (<div className='login'>
+          <Header
+           black={true}
+          />
+          <div className='login-wrap'>
+            <h2 className='login-title'>登录/注册</h2>
+          </div>
+          <div className='login-enter'>
+            <div className='login-3d'>
+              <div className='login-inner login-form'>登录</div>
+              <div className='login-inner regest-form'>注册</div>
+            </div>
+          </div>
+        </div>)
     }
 }
 
